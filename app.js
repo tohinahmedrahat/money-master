d0a8a74f0bb467dea520e5417bb18fa7dd17e35d
tohinahmedrahat <rahat4248@gmail.com>
2022-08-21 17:40:46 +0600

    some validation added

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ function setValue(id, value) {
   const getValue = document.getElementById(id);
   getValue.innerText = value;
 }
+ // error handle message
+ const errorMassege = "please input value to input field or enter 0";
+ const error = document.getElementById("error");
 
 // add eventlistener to calculet button
 const calculetBtn = document.getElementById("cal");
 
 calculetBtn.addEventListener("click", function () {
+   
   // get all input value
   const income =document.getElementById("income");
   const incomeString = income.value;
@@ -26,30 +30,37 @@ calculetBtn.addEventListener("click", function () {
   const food = getInputValue("food");
   const rent = getInputValue("rent");
   const clothes = getInputValue("cloth");
+//   get total value
   const expense = food + rent + clothes;
+//   get reaming balance
   const balance =incomeValue  - expense;
+//   check all value in number or not
   if (isNaN(incomeValue) || isNaN(food) || isNaN(rent) || isNaN(clothes)) {
-    alert("please input valid numer");
+    error.innerText = errorMassege 
   } else if (incomeValue < expense) {
     alert("you expense is begest then income");
   } else {
     setValue("total", expense);
     setValue("balance", balance);
+    error.innerText = "";
   }
 });
 
 // add saving button
 document.getElementById("saveBtn").addEventListener("click", function () {
+    // get income value
   const income =document.getElementById("income");
   const incomeString = income.value;
   const incomeValue = parseFloat(incomeString);
+//   get total balnce remaing
   const balance =document.getElementById("balance");
   const balanceString = balance.innerText;
   const balanceValue = parseFloat(balanceString);
-  console.log(balanceString)
+//   get saving value
   const saving = getInputValue("save");
+//   check input value is NaN or not;
   if(isNaN(saving)){
-    alert("please valid saving number")
+    error.innerText = errorMassege;
     return;
   }else{
     const savingMoney = incomeValue * (saving / 100);
@@ -57,6 +68,7 @@ document.getElementById("saveBtn").addEventListener("click", function () {
     setValue("savingAmount",savingMoney);
     setValue("remain",remainBalance)
     income.value = "";
+    error.innerText = "";
   }
  
 });
